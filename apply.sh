fbbe849fab53d77caaf5b1a8e10fbc4bde9eaf0b
peterhack <peter.hack@dynatrace.com>
2018-12-08 18:26:48 -0500

    fixed apply.sh

--- apply.sh
+++ apply.sh
@@ -8,25 +8,25 @@
 
 if [ -z "$1" ]; then
   echo "Usage: Arg 1 needs to be a valid apiToken entry [apiToken=...]"
-  echo 'ex: ./apply.sh $DTAPI $DTPASS $TENANTURL'
+  echo 'ex: ./apply.sh $DTAPI $DTPASS $DTENV'
   exit 1
 fi
 
 if [ -z "$2" ]; then
   echo "Usage: Arg 2 needs to be a valid paasToken entry [paasToken=...]"
-  echo 'ex: ./apply.sh $DTAPI $DTPASS $TENANTURL'
+  echo 'ex: ./apply.sh $DTAPI $DTPASS $DTENV'
   exit 1
 fi
 
 if [ -z "$3" ]; then
-  echo "Usage: TENANTURL is required to configure the OneAgent Operator"
-  echo "example: /> export TENANTURL='abc12345.dynatrace.live.com'"
-  echo "            --for Managed-- /> export TENANTURL='host_url/e/environmentid' "
-  echo 'ex: ./apply.sh $DTAPI $DTPASS $TENANTURL'
+  echo "Usage: Environment ID is required to configure the OneAgent Operator"
+  echo "example: /> export DTENV='abc12345'"
+  echo "            --for Managed-- /> export DTENV='host_url/e/environmentid' "
+  echo 'ex: ./apply.sh $DTAPI $DTPASS $DTENV'
   exit 1
 fi
 
-sed -i 's/ENVIRONMENTID.live.dynatrace.com/'"$3"'/' cr.yaml
+sed -i 's/ENVIRONMENTID/'"$3"'/' cr.yaml
 oc create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/master/deploy/openshift.yaml
 oc -n dynatrace create secret generic oneagent --from-literal="$1" --from-literal="$2"
 oc create -f ./cr.yaml
