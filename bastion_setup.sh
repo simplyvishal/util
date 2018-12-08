630300969a0ff44332ed8a7addb1a661bbe9f7d0
peterhack <peter.hack@dynatrace.com>
2018-12-08 12:22:46 -0500

    changed cp to mv

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Dynatrace OneAgent Operator
 git clone https://github.com/dynatrace/dynatrace-oneagent-operator.git
-cp ./apply.sh ./dynatrace-oneagent-operator/deploy/apply.sh
-cp ./cleanup.sh ./dynatrace-oneagent-operator/deploy/cleanup.sh
+mv ./apply.sh ./dynatrace-oneagent-operator/deploy/apply.sh
+mv ./cleanup.sh ./dynatrace-oneagent-operator/deploy/cleanup.sh
 
 # Utility configuration
 sudo ./hub-linux-amd64-2.6.0/install
