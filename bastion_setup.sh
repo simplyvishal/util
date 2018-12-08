c4171589c9ed503c01e89216195424b2f78f6d92
peterhack <peter.hack@dynatrace.com>
2018-12-08 18:00:49 -0500

    added acm repo

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -7,3 +7,6 @@ mv ./cleanup.sh ./dynatrace-oneagent-operator/deploy/cleanup.sh
 # Utility configuration
 sudo ./hub-linux-amd64-2.6.0/install
 sudo mv ./jq-linux64 /usr/sbin/jq
+
+git clone https://github.com/dt-seattle-acm-setup/provision-acm-openshift.git
+
