4198f1fd6483804f4fa993e10b9c1494b0b6e328
peterhack <peter.hack@dynatrace.com>
2018-12-09 11:01:50 -0500

    added 1 to master in bastion script

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -13,7 +13,7 @@ git clone https://github.com/dt-seattle-acm-setup/provision-acm-openshift.git
 mv ./creds.sav ./provision-acm-openshift/scripts/creds.json
 
 # log into the OpenShift cluster
-oc login https://master:443
+oc login https://master1:443
 
 # make bastion_setup.sh non-executable once completed
 chmod 666 bastion_setup.sh
