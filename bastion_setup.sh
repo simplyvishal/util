3732b55e78f7cf1cafc4e78274c77ad6699b03cb
peterhack <peter.hack@dynatrace.com>
2019-01-07 16:21:35 -0500

    fixed path typo

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -33,4 +33,4 @@ echo "Please log into your OpenShift cluster"
 oc login https://master1:443
 
 # make bastion_setup.sh non-executable once completed
-chmod 666 bastion_setup.sh
+chmod 666 ~/util/bastion_setup.sh
