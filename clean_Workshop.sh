6acfb97f879f08c3341667f6aa5c7096356eebfb
peterhack <peter.hack@dynatrace.com>
2019-01-08 04:08:02 +0000

    added cleanup file

---
+++ clean_Workshop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# THIS SCRIPT CAN BE USED TO CLEAN UP THE SOCKSHOP DEPLOYMENT #
+oc delete projects cicd dev staging production sockshop-registry
+oc adm policy remove-scc-from-group hostpath system:authenticated
+oc delete -f ../manifests/oc-scc-hostpath.yml
+oc delete -f ../manifests/k8s-jenkins-rbac.yml
+oc delete -f ../manifests/k8s-jenkins-pvcs.yml
