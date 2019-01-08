e698582fd91704b0480bc9f5e5e423cb75469860
peterhack <peter.hack@dynatrace.com>
2019-01-08 04:26:13 +0000

    added cleanup for istio

--- clean_Workshop.sh
+++ clean_Workshop.sh
@@ -5,3 +5,8 @@ oc delete projects cicd dev staging production sockshop-registry
 oc adm policy remove-scc-from-group hostpath system:authenticated
 oc delete -f ../manifests/oc-scc-hostpath.yml
 oc delete -f ../manifests/k8s-jenkins-rbac.yml
+oc delete -f ../manifests/istio/istio-gateway.yml
+oc delete -f ../manifests/isito/istio-installation.yml
+oc adm policy remove-scc-from-user anyuid -z default -n production
+oc adm policy remove-scc-from-user privileged -z default -n production
+oc delete projects cicd dev staging production sockshop-registry istio-system istio-operator
